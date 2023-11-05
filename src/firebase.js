4a5491b34a3b6d60321d5a45c03784e35f478761
Jonel Hatwell <jonel.hatwell@gmail.com>
2023-11-05 11:27:45 +0800

    update env

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import {getAuth} from "firebase/auth";
 
+console.log(import.meta.env.VITE_REACT_APIKEY);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDSpa_JOct9gYYKEeeuL3H7ZhGw4dUdBr0",
-  authDomain: "react-80ff9.firebaseapp.com",
-  projectId: "react-80ff9",
-  storageBucket: "react-80ff9.appspot.com",
-  messagingSenderId: "167354454109",
-  appId: "1:167354454109:web:58a68e153702dff0427db4"
+  apiKey: import.meta.env.VITE_REACT_APIKEY,
+  authDomain: import.meta.env.VITE_REACT_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_REACT_PROJECTID,
+  storageBucket: import.meta.env.VITE_REACT_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_REACT_APPID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
